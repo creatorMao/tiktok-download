ba78c05eeed877fc5e74f576981b145854dd9228
Zhiwu Mao <zwmao@outlook.com>
2023-02-06 20:52:42 +0800

    增加信息中心配置

--- config.js
+++ config.js
@@ -2,8 +2,13 @@ const homeUrlPrefix = "https://www.douyin.com/user/"
 const retryCount = 3 //重试次数
 const apiPort = 3000
 const dbFilePath = './Database/basedb.db'
+const newsCenter = {
+  groupId: 'tiktok-download-status',
+  url: ''
+}
 
 export {
+  newsCenter,
   homeUrlPrefix,
   retryCount,
   apiPort,
