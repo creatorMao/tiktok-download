877df92950f60dc716b94a1dc46a364868587cde
Zhiwu Mao <zwmao@outlook.com>
2023-02-08 17:14:00 +0800

    完善下载机制，防止报错

--- Helper/fsHelper.js
+++ Helper/fsHelper.js
@@ -1,6 +1,7 @@
-import fs, { existsSync } from 'fs'
+import fs, { existsSync, write } from 'fs'
 import path from 'path'
 import axios from 'axios'
+import { retryCount } from '../Config/config.js'
 import { fileURLToPath } from 'url'
 const filename = fileURLToPath(import.meta.url) // 这里不能声明__filename,因为已经有内部的__filename了，重复声明会报错
 const _dirname = path.dirname(filename)
@@ -12,27 +13,87 @@ const createDir = (path) => {
   }
 }
 
-const saveFile = async (url, filePath, fileName) => {
+const deleteFile = (path) => {
+  if (existsSync(path)) {
+    fs.unlinkSync(path);
+  }
+}
+
+const saveFile = async (url, filePath, fileName, retryFlag = true, retryCountTotal = 3, currentRetryCount = 0) => {
+  if (!retryCountTotal) {
+    retryCountTotal = retryCount
+  }
+
   const filePathAbs = path.join(rootPath, filePath + "/" + fileName)
+  let res = {
+    msg: '',
+    existFlag: false,
+    downloadSuccessFlag: false
+  }
+  let writer = undefined
 
   if (fs.existsSync(filePathAbs)) {
-    console.log('文件已存在！');
-    return "文件已存在！"
+    res.msg = "文件已存在！"
+    res.existFlag = true
+    res.downloadSuccessFlag = true
+    return res
   }
   else {
-    createDir(path.join(rootPath, filePath))
-    const writer = fs.createWriteStream(filePathAbs)
+    try {
+      createDir(path.join(rootPath, filePath))
 
-    const response = await axios({
-      url,
-      method: 'GET',
-      responseType: 'stream'
-    })
-    response.data.pipe(writer)
-    return new Promise((resolve, reject) => {
-      writer.on('finish', resolve)
-      writer.on('error', reject)
-    })
+      writer = fs.createWriteStream(filePathAbs)
+      const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+      })
+      response.data.pipe(writer)
+
+      return new Promise((resolve, reject) => {
+        writer.on('finish', () => {
+          res.msg = "文件下载成功！"
+          res.existFlag = false
+          res.downloadSuccessFlag = true
+          resolve(res);
+        })
+        writer.on('error', async (err, writer) => {
+          if (writer) {
+            writer.end();
+            writer.destroy();
+            deleteFile(filePathAbs);
+          }
+
+          if (retryFlag && currentRetryCount < retryCountTotal) {
+            console.log(`可能因为网络原因，第${currentRetryCount + 1}次下载失败，正在进行第${currentRetryCount + 1}次尝试！`);
+            resolve(await saveFile(url, filePath, fileName, true, undefined, currentRetryCount + 1));
+          }
+          else {
+            res.msg = "文件下载失败！"
+            res.existFlag = false
+            res.downloadSuccessFlag = false
+            resolve(res);
+          }
+        })
+      })
+    }
+    catch (err) {
+      if (writer) {
+        writer.end();
+        writer.destroy();
+        deleteFile(filePathAbs);
+      }
+      if (retryFlag && currentRetryCount < retryCountTotal) {
+        console.log(`可能因为网络原因，第${currentRetryCount + 1}次下载失败，正在进行第${currentRetryCount + 2}次尝试！`);
+        return await saveFile(url, filePath, fileName, true, retryCountTotal, currentRetryCount + 1);
+      }
+      else {
+        res.msg = err.message
+        res.existFlag = false
+        res.downloadSuccessFlag = false
+        return res
+      }
+    }
   }
 }
 
--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-let a = Date.now()
-console.log('begin');
-for (let i = 0; i <= 2000000000; i++) {
-  if (i == 2000000000) {
-    let b = Date.now()
-    console.log('end');
-    let c = b - a
-    console.log((0 / 1000).toFixed(0));
-  }
-}
+// let a = Date.now()
+// console.log('begin');
+// for (let i = 0; i <= 2000000000; i++) {
+//   if (i == 2000000000) {
+//     let b = Date.now()
+//     console.log('end');
+//     let c = b - a
+//     console.log((0 / 1000).toFixed(0));
+//   }
+// }
 
 
 // import { getSecond, getNowDate } from './Helper/dateHelper.js'
 // console.log(getNowDate());
+
+import { saveFile } from './Helper/fsHelper.js'
+console.log(2);
+const res = await saveFile('https://t8.baidu.com/it/u=3616464563,3199287374&fm=217&app=125&size=f242,162&n=0&g=0n&f=JPEG?s=7FEAAB561F5F7CC80C490BFB0300903C&sec=1675929937&t=aafa7485378d4856ce31b0fcd97a1de0', './Data/test', '1.jpg')
+console.log(res);
+console.log(1);
